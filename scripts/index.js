3618ed3034548a6f2e8ca0598e33b17ead65a475
Dnadit <yuhyun.kang07@gmail.com>
2023-01-20 15:28:59 +0900

    주사위게임 완성

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,34 @@
+//전역변수 선언
+let n ;
+
+//주사위 번호 선택
 const showDice = () => {
-    const n = Math.floor(Math.random() * 6) + 1;
-    const s1 = document.querySelector("#s1") ;   
-    s1.innerHTML = `<img src="./imges/${n}.png">`        
+    n = Math.floor(Math.random() * 6) + 1;
+    console.log(n);
+    show("none", "block", "none", "none", "block");
+}
+
+//주사위 번호 결과 확인
+const checkDice = () => {
+    const radios = document.querySelectorAll("input[type=radio]");
+    show("block", "none", "block", "block", "none");
+    div1.innerHTML = `<img src="./imges/${n}.png">`;
+
+    //사용자가 선택한 숫자
+    let usern ;
+    for(let r of radios) {
+        if(r.checked) {
+            usern = r.value ;
+            break;            
+        }
+    }
+    if(n == usern) {
+        div3.innerHTML = `<img src="./imges/o.png">`;
+    } else {
+        div3.innerHTML = `<img src="./imges/x.png">`;
+    }
+    document.querySelector("form").reset();
+    console.log(n, usern);
 }
 
 //요소 보이기 함수
@@ -21,18 +48,5 @@ const show = (dspS1, dspS2, dspS3, dspBt1, dspBt2) => {
 
 //DOM 로드가 된 후
 document.addEventListener("DOMContentLoaded", () => {
-    show("none", "block", "none", "block", "block");
+    show("none", "none", "none", "block", "none");
 })
-
-const checkDice = () => {
-    const s3 = document.querySelector("#s3");
-    const chooseDice = document.getElementsByName("num");
-    console.log(chooseDice);
-    chooseDice.foreach((node)=>{
-        if(node.checked) {
-            s3.innerHTML = `<img src="./imges/o.png">`;
-        } else {
-            s3.innerHTML = `<img src="./imges/x.png">`;
-        }
-    })   
-}
